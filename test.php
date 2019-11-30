253c02ab52d5e169fbcdcbf5838ca5c4908c759c
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-01 04:41:59 +0700

    test exception

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
-
-use Telegram\Telegram;
-
-$telegram = new Telegram("");
-
-
+use Telegram\Bot\Client;
+try {
+    $telegram = new Client('1061xxxxx:AAxxxxx');
+}
+catch(Exception $e) {
+    fprintf(STDERR, "%s break :%s\n", $e->getMessage(), $telegram->__getBreakpoints());
+    exit(-1);
+}
